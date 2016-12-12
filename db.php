255cd6c028a2762f57a3c91fb33456cde01d3740
Joseph Nwachukwu <josephnwachukwu@gmail.com>
2016-12-12 03:03:29 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-	$hostname = "localhost";
-	$database = "louisdag_fastflowers";
-	$username = "louisdag_joseph";
-	$password = "aventador1";
+	$hostname = "hostname";
+	$database = "database_name";
+	$username = "database_username";
+	$password = "database_password";
 	
 	$login = mysql_connect ( $hostname, $username, $password ) or trigger_error ( mysql_error(),E_USER_ERROR );
 	mysql_select_db ( $database );
